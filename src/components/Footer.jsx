383988fc42fbf5ddd383dc379e7b235c41b52292
refugiocampestreaguazul-commits <refugiocampestreaguazul@gmail.com>
2026-05-29 14:27:06 -0500

    Update landing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -103,7 +103,7 @@ export default function Footer() {
               <Mail className="h-5 w-5 text-cyan-400" />
 
               <span>
-                contacto@cdapro.com
+                informacion.makrosoft@gmail
               </span>
 
             </div>
